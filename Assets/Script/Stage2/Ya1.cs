e7555db7b6c162b6daf733007d9f73f2d5b53fc8
SZ-tu <zizu.bluesky@gmail.com>
2023-08-28 21:20:40 +0900

    Stage2作成
    Stage2を作成しました
    Stage1と同様に不具合や修正が必要な個所があれば見つけ次第対応します

---
+++ Assets/Script/Stage2/Ya1.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ya1 : MonoBehaviour
+{
+    public int ya1 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //座標を取得
+        Transform myTransform = this.transform;
+
+        //座標の変更
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Vector3 pos = myTransform.position;
+            pos.x -= 2f;
+            myTransform.position = pos;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Vector3 pos = myTransform.position;
+            pos.x += 2f;
+            myTransform.position = pos;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ya"))
+        {
+            // 触れたobjの親を移動床にする
+            other.transform.SetParent(transform);
+            ya1 = 1;
+            Debug.Log("「や」が正しく配置されたよ！！");
+        }
+    }
+}
