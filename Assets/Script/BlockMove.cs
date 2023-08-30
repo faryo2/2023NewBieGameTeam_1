eaa91d6270995c9118b8fcfdf8ba9b194de28ef5
SZ-tu <zizu.bluesky@gmail.com>
2023-08-30 15:53:12 +0900

    Block削除
    すでに土台に付いているブロックに触れたブロックは削除されるようにしました

--- Assets/Script/BlockMove.cs
+++ Assets/Script/BlockMove.cs
@@ -4,25 +4,51 @@ using UnityEngine;
 
 public class BlockMove : MonoBehaviour
 {
-    [SerializeField] private float blockSpeed;
     [SerializeField] private float blockLifeTime;
+    [SerializeField] private float blockLife;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.Translate(new Vector3(0, -blockSpeed, 0) * Time.deltaTime);
-
-        //BlockLifeTime秒後にオブジェクトを消す
-        blockLifeTime = blockLifeTime - Time.deltaTime;
-        if (blockLifeTime < 0)
-        {
-            Destroy(this.gameObject);
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (blockLife < 0)
+        {
+            //BlockLifeTime秒後にオブジェクトを消す
+            blockLifeTime = blockLifeTime - Time.deltaTime;
+            if (blockLifeTime < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Board"))
+        {
+            blockLife = 1;
+        }
+        if (other.gameObject.CompareTag("Ha") && blockLife < 0)
+        {
+            Destroy(this.gameObject);
+        }
+        if (other.gameObject.CompareTag("Chi") && blockLife < 0)
+        {
+            Destroy(this.gameObject);
+        }
+        if (other.gameObject.CompareTag("Ya") && blockLife < 0)
+        {
+            Destroy(this.gameObject);
+        }
+        if (other.gameObject.CompareTag("Block") && blockLife < 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
