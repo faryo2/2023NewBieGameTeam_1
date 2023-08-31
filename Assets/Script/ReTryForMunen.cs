766191cc537cc495265ebd6c9c29de33c819c055
faryo2 <faryo0204@gmail.com>
2023-08-31 15:37:06 +0900

    モデルの配置

---
+++ Assets/Script/ReTryForMunen.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReTryForMunen : MonoBehaviour
+{
+    // 前のシーンに戻るための関数
+    public void GoBackToPreviousScene()
+    {
+        // 現在のシーンのインデックスを取得
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // 前のシーンのインデックスを計算
+        int previousSceneIndex = currentSceneIndex - 2;
+
+        // インデックスが0未満にならないように調整
+        if (previousSceneIndex < 0)
+        {
+            previousSceneIndex = 0;
+        }
+
+        // 前のシーンに移動
+        SceneManager.LoadScene(previousSceneIndex);
+    }
+}
